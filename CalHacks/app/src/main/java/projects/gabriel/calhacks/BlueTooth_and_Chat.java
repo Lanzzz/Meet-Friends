fd9e3c9f718f8fc5f7255c54a37e75febda70ca0
Landon <landon159@sbcglobal.net>
2015-10-10 16:21:33 -0700

    small additions and comments

--- CalHacks/app/src/main/java/projects/gabriel/calhacks/BlueTooth_and_Chat.java
+++ CalHacks/app/src/main/java/projects/gabriel/calhacks/BlueTooth_and_Chat.java
@@ -1,11 +1,22 @@
 package projects.gabriel.calhacks;
 
+        //important generic API for bluetooth.
+//http://developer.android.com/guide/topics/connectivity/bluetooth.html
 import android.bluetooth.*;
+
+
 /**
  * Created by Landon on 10/10/2015.
  */
 public class BlueTooth_and_Chat {
 
+    BluetoothAdapter bta;
+
+    //The server socket is a single socket that people can queue up to.
+    BluetoothServerSocket btss;
+
+    //bts is current healthy connections, which should be a finite limit.
+    BluetoothSocket bts[];
 
 
 }
