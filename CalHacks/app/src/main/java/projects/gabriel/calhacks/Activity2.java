de3fb1a0c78351527dc74dc673707b5624210c90
Landon <landon159@sbcglobal.net>
2015-10-10 14:33:01 -0700

    MostProgress attempted Upload1.

--- CalHacks/app/src/main/java/projects/gabriel/calhacks/Activity2.java
+++ CalHacks/app/src/main/java/projects/gabriel/calhacks/Activity2.java
@@ -1,39 +1,29 @@
 package projects.gabriel.calhacks;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
-
-    //Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        //button = (Button)findViewById(R.id.correct_button); // weird
-        //button.setOnClickListener(this);
-    }
-    /*
-    public void buttonClick()
-    {
-        startActivity(new Intent("projects.gabriel.calhacks.Activity2)"));
-    }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    public void onClick(View v)
-    {
-        switch (v.getId())
-        {
-            case R.id.correct_button:
-            {
-                buttonClick();
-                break;
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-        }
+        });
     }
-    */
 
 }
--- CalHacks/app/src/main/java/projects/gabriel/calhacks/continue_button.java
+++ CalHacks/app/src/main/java/projects/gabriel/calhacks/continue_button.java
@@ -6,38 +6,36 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by gabriel on 10/10/2015.
+ */
+public class continue_button extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        /*final TextView firstTextView = (TextView) findViewById(R.id.textView);
-
-        final Button firstButton = (Button) findViewById(R.id.button);
-
-        firstButton.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                firstTextView.setText("You Clicked");
+                Snackbar.make(view, "You clicked", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-        });*/
-        //startActivity(new Intent(getApplicationContext(), continue_button.class));
-
+        });
     }
     public void buttonOnClick(View v){
 
         final Button firstButton = (Button) findViewById(R.id.button);
-        startActivity(new Intent(getApplicationContext(), Activity2.class));
+        startActivity(new Intent(getApplicationContext(), continue_button.class));
     }
 
     @Override
@@ -62,3 +60,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
