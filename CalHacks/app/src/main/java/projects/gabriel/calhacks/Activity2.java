103bed5caca31ed03334c70cfe9d94d3abc314fa
Landon <landon159@sbcglobal.net>
2015-10-18 16:19:48 -0700

    add history

---
+++ CalHacks/app/src/main/java/projects/gabriel/calhacks/Activity2.java
@@ -0,0 +1,30 @@
+package projects.gabriel.calhacks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    public void nextSlide(View v){
+
+        final Button Button2 = (Button) findViewById(R.id.button);
+        startActivity(new Intent(this, OptionSlide.class));
+    }
+
+}
